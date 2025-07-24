47e6f1543e8d89f1f2022f16af6a81577a1634dd
Adam <adammhal@gmail.com>
2025-07-24 18:30:41 -0400

    Initial Commit

---
+++ Dionysus/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Dionysus
+//
+//  Created by Adam Mhal on 7/24/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ DionysusTests/DionysusTests.swift
@@ -0,0 +1,17 @@
+//
+//  DionysusTests.swift
+//  DionysusTests
+//
+//  Created by Adam Mhal on 7/24/25.
+//
+
+import Testing
+@testable import Dionysus
+
+struct DionysusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
