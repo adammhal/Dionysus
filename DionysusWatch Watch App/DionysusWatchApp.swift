faad2e956bce53ee0bfa5d67a1f4bafe55776f13
Adam <adammhal@gmail.com>
2025-09-22 15:17:36 -0400

    Add base for Mac, Watch, and iPad apps

---
+++ DionysusWatch Watch App/DionysusWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  DionysusWatchApp.swift
+//  DionysusWatch Watch App
+//
+//  Created by Adam Mhal on 9/22/25.
+//
+
+import SwiftUI
+
+@main
+struct DionysusWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
